edc0a6fcd51c8215e0a29ca772b7029f6bba3a9a
Adé Mochtar <ade@instruqt.com>
2021-07-02 16:42:48 +0200

    Add option to add k3s startup flags

--- files/k3s-start.sh
+++ files/k3s-start.sh
@@ -2,6 +2,7 @@
 
 METADATA_ENDPOINT="http://metadata.google.internal/computeMetadata/v1/instance/attributes"
 K3S_CONTROL_PLANE_HOSTNAME=$(curl -sfH "Metadata-Flavor: Google" "${METADATA_ENDPOINT}/K3S_CONTROL_PLANE_HOSTNAME")
+K3S_FLAGS=$(curl -sfH "Metadata-Flavor: Google" "${METADATA_ENDPOINT}/K3S_FLAGS")
 
 K3S_CMD=server
 K3S_NODE_NAME=$(hostname)
@@ -41,4 +42,4 @@ else
     systemctl start kube-dashboard &
 fi
 
-/usr/local/bin/k3s "$K3S_CMD"
+/usr/local/bin/k3s "$K3S_CMD" ${K3S_FLAGS}
