de8cef15065643f7e3a0caa0a7f281e70fb19b72
Adé Mochtar <ade@instruqt.com>
2021-08-24 08:49:38 +0200

    Support INSTALL_K3S_EXEC env var

--- files/k3s-start.sh
+++ files/k3s-start.sh
@@ -3,6 +3,7 @@
 METADATA_ENDPOINT="http://metadata.google.internal/computeMetadata/v1/instance/attributes"
 K3S_CONTROL_PLANE_HOSTNAME=$(curl -sfH "Metadata-Flavor: Google" "${METADATA_ENDPOINT}/K3S_CONTROL_PLANE_HOSTNAME")
 K3S_FLAGS=$(curl -sfH "Metadata-Flavor: Google" "${METADATA_ENDPOINT}/K3S_FLAGS")
+INSTALL_K3S_EXEC=$(curl -sfH "Metadata-Flavor: Google" "${METADATA_ENDPOINT}/INSTALL_K3S_EXEC")
 
 K3S_CMD=server
 K3S_NODE_NAME=$(hostname)
@@ -42,4 +43,4 @@ else
     systemctl start kube-dashboard &
 fi
 
-/usr/local/bin/k3s "$K3S_CMD" ${K3S_FLAGS}
+/usr/local/bin/k3s "$K3S_CMD" ${K3S_FLAGS} ${INSTALL_K3S_EXEC}
